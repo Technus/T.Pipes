bb4bf2aa68a6b76363720c32b5c746efaa328bf8
Daniel Peczkowski (DAPC) <daniel112092@gmail.com>
2023-12-08 15:02:23 +0100

    Add test Delegating pipe

---
+++ T.Pipes.Test/PipeDelegationTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T.Pipes.Test.Abstractions;
+
+namespace T.Pipes.Test
+{
+  public class PipeDelegationTests
+  {
+    [Fact]
+    public async Task Starts()
+    {
+    }
+  }
+
+  [PipeServe(typeof(IAbstract))]
+  internal sealed partial class ServerCallback : DelegatingPipeServerCallback<ServerCallback>
+  {
+    public ServerCallback(H.Pipes.PipeServer<PipeMessage> pipe) : base(pipe)
+    {
+    }
+  }
+
+  internal sealed class Server : DelegatingPipeServer<ServerCallback>
+  {
+    public Server(string pipe, ServerCallback callback) : base(pipe, callback)
+    {
+    }
+
+    public Server(H.Pipes.PipeServer<PipeMessage> pipe, ServerCallback callback) : base(pipe, callback)
+    {
+    }
+  }
+
+  [PipeUse(typeof(IAbstract))]
+  internal sealed partial class ClientCallback<TTarget> : DelegatingPipeClientCallback<TTarget, ClientCallback<TTarget>>
+    where TTarget : IAbstract
+  {
+    public ClientCallback(H.Pipes.PipeClient<PipeMessage> pipe, TTarget target) : base(pipe, target)
+    {
+    }
+  }
+
+  internal sealed class Client<TTarget> : DelegatingPipeClient<TTarget, ClientCallback<TTarget>>
+    where TTarget : IAbstract
+  {
+    public Client(string pipe, ClientCallback<TTarget> callback) : base(pipe, callback)
+    {
+    }
+
+    public Client(H.Pipes.PipeClient<PipeMessage> pipe, ClientCallback<TTarget> callback) : base(pipe, callback)
+    {
+    }
+  }
+}
